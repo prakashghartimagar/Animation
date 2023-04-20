d5b46945a3817f6306f282257f18110fcb32c1ca
prakashghartimagar <122581776+prakashghartimagar@users.noreply.github.com>
2023-04-20 10:04:20 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+gsap.from('.pizza-img', { duration: 1, y: '-100%', delay:1})
+gsap.from('.ux-strategy',{duration: 1, y: '-100%',})
+gsap.from('.dominos-bread',{duration: 1, delay:1, y: '-100%',})
+gsap.from('.dominos-bread1',{duration: 1, delay:1, y: '100%',})
+gsap.from('.ux-mob',{duration: 1, y: '100%',})
+
+
+
+gsap.from('.measure-total-image', { duration: 2, y: '100%', delay:0.2})
+gsap.from('.nasa-fitness-image',{duration: 2, y: '-100%', delay:0.2,})
+
+
+
+gsap.from('.vame-image1', { duration: 2, y: '-100%', delay:0.2})
+gsap.from('.vame-image2',{duration: 2, y: '100%', delay:0.2,})
+
+
+gsap.to('.karavan-social-image', { duration: 3, y: '-100%',})
+gsap.to('.social-networking-image',{duration: 3, y: '-100%',})
+gsap.to('.developers-for-social-media-image', {duration: 3, x: 300})
+gsap.to('.karavan_2-image',{duration: 2, y: '100%',})
+gsap.to('.karavan-social-networking-image2',{duration: 3, y: '100%',ease: 'power2.out'})
+
+
+gsap.from('.nexgtv-entertainment-image', { duration: 1, y: '150%'})
+gsap.from('.nexgtv-mobile-app-image',{duration: 1, y: '150%'})
+
+
+
+gsap.from('.melltoo-img1', { duration: 1, x: '150%'})
+gsap.from('.melltoo-img2',{duration: 1, x: '-150%'})
+
+
